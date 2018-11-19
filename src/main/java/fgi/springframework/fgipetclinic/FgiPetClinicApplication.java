f69b801f99a03fa3b5d0ad1aeeee0217d2fcca69
Florian.GALLI <Florian.GALLI@FRL00965.DSV.COM>
2018-11-19 13:43:56 +0100

    Initial Project Add

---
+++ src/main/java/fgi/springframework/fgipetclinic/FgiPetClinicApplication.java
@@ -0,0 +1,12 @@
+package fgi.springframework.fgipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgiPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FgiPetClinicApplication.class, args);
+	}
+}
