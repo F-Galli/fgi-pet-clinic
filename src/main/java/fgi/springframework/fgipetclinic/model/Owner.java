6960effa5f5b689875ad65f69ad3275f130c97a4
Florian.GALLI <Florian.GALLI@FRL00965.DSV.COM>
2018-12-03 12:35:32 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/fgi/springframework/fgipetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package fgi.springframework.fgipetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/fgi/springframework/fgipetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package fgi.springframework.fgipetclinic.model;
+
+public class Vet extends Person {
+
+}
